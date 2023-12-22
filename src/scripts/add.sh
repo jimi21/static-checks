961df1fa7a7d6862ca63873d560aa8681b1b7d75
Jimi Patel <Jimi_Patel@bose.com>
2023-12-22 14:44:37 +0530

    updated config file

--- src/scripts/add.sh
+++ src/scripts/add.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This example uses envsubst to support variable substitution in the string parameter type.
-# https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
-
-# If for any reason the TO variable is not set, default to "World"
-echo $(("${PARAM_NUMA}" + "${PARAM_NUMB}"))
+ADD_RESULT=$(("${PARAM_NUMA}" + "${PARAM_NUMB}"))
+echo $ADD_RESULT
+touch /tmp/ADD_RESULT.txt
+echo $ADD_RESULT > /tmp/ADD_RESULT.txt
